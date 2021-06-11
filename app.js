c4a8998b5c4960bb4541bfcffa1ef564ba607d2a
Euntae Kim <hurima90@gmail.com>
2021-06-12 01:50:46 +0900

    Feat: create function LiModel and ouput time using moment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const server = http.createServer(app); // express는 http를 통해서 실행됨
 const socketIO = require("socket.io");
+const moment = require("moment");
 
 const io = socketIO(server);
 
@@ -13,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
     socket.on("chatting", (data) => {
-        io.emit("chatting", data)
+        const { name, msg } = data;
+        io.emit("chatting", {
+            name: name,
+            msg: msg,
+            time: moment(new Date()).format("H:mm A")
+        })
     })
 });
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -6,6 +6,7 @@ const nickname = document.querySelector("#nickname");
 const chatList = document.querySelector(".chatting-list");
 const chatInput = document.querySelector(".chatting-input");
 const sendButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
 
 sendButton.addEventListener("click", () => {
     const param = {
@@ -16,10 +17,33 @@ sendButton.addEventListener("click", () => {
 });
 
 // 서버에서 보낸 내용 확인
-socket.on("chatting", (data) => { 
-    const li = document.createElement("li");
-    li.innerText = `${data.name}님이 ${data.msg}`;
-    chatList.appendChild(li);
-})
+socket.on("chatting", (data) => {
+    const { name, msg, time } = data; // 넘어온 데이터중 필요한 부분만 변수로 저장
+    const item = new LiModel(name, msg, time); // LiModel을 인스턴스화 (초기화)
+    item.makeLi()
+    displayContainer.scrollTo(0, displayContainer.scrollHeight)
+});
+
+function LiModel(name, msg, time){
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    // makeLi 함수로 접근
+    this.makeLi = () => {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent" : "received")
+        const dom = `
+            <span class="profile">
+                <img class="image" src="https://placeimg.com/50/50/any" alt="">
+                <span class="user">${this.name}</span>
+            </span>
+            <span class="message">${this.msg}</span>
+            <span class="time">${this.time}</span>
+        `;
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+    };
+};
 
 console.log(socket);

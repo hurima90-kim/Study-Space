0d87ba999757a4f2c1797aeea6ce9dbe1be8c77a
Euntae Kim <hurima90@gmail.com>
2021-06-15 21:22:17 +0900

    Protect secret infomation

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jasonkim:abcd1234@cluster0.fofyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('Mongo DB Connected...')).catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! 머리가 존나게 아프다'))
+
+
+app.post('/register', (req, res) => {
+    // 회원가입정보를 client에서 가져오면 Database에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))

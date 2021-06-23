27ef292f9d0d6235db96d0b577f5d61bf9c1eb66
Euntae Kim <hurima90@gmail.com>
2021-06-24 02:01:57 +0900

    Feat: Setting Redux and add register, login, logout function

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createProxyMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider
+      store={createProxyMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__() 
+      )}
+    >
+      <App />
+    </Provider>
+    , document.getElementById('root')
 );

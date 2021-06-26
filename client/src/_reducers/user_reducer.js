42f9546442bb553c1a9d861a5f2e69ce22151293
Euntae Kim <hurima90@gmail.com>
2021-06-26 20:25:41 +0900

    Feat: apply redux for login, logout, register

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,5 +1,5 @@
 import {
-    LOGIN_USER, REGISTER_USER
+    LOGIN_USER, REGISTER_USER, AUTH_USER
 } from '../_actions/types';
 
 
@@ -12,6 +12,9 @@ export default function(state = {}, action){
             break;
         case REGISTER_USER:
             return {...state, register: action.payload}
+            break;
+        case AUTH_USER:
+            return {...state, userData: action.payload}
             break;    
         default:
             return state;
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const config = require('./config/key')
 const { User } = require('./models/User')
 const { auth } = require('./middleware/auth')
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 app.use(cookieParser())
@@ -22,13 +22,16 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World! 머리가 존나게 아프다'))
 
 
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
+
+
 app.post('/api/users/register', (req, res) => {
 
     // 회원가입정보를 client에서 가져오면 Database에 넣어준다.
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
-        if(err) return res.json({success: false, err})
+        if (err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
         })
@@ -39,7 +42,7 @@ app.post('/api/users/login', (req, res) => {
 
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다
     User.findOne({ email: req.body.email }, (err, user) => {
-        if(!user){
+        if (!user) {
             return res.json({
                 loginSuccess: false,
                 message: '제공된 이메일에 해당하는 유저가 없습니다.'
@@ -47,21 +50,21 @@ app.post('/api/users/login', (req, res) => {
         }
         // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
         user.comparePassword(req.body.password, (err, isMatch) => {
-    
-        if (!isMatch)
-            return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-        
-        //비밀번호 까지 맞다면 토큰을 생성하기.
-        user.generateToken((err, user) => {
-            if (err) return res.status(400).send(err);
-        
-            // 토큰을 저장한다. 쿠키 , 로컬스토리지 
-            res.cookie("x_auth", user.token)
-            .status(200)
-            .json({ loginSuccess: true, userId: user._id })
+
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+            //비밀번호 까지 맞다면 토큰을 생성하기.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 저장한다. 쿠키 , 로컬스토리지 
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
             })
         })
-    })  
+    })
 })
 
 app.get('/api/users/auth', auth, (req, res) => {
@@ -82,7 +85,7 @@ app.get('/api/users/auth', auth, (req, res) => {
 app.get('/api/users/logout', auth, (req, res) => {
     User.findOneAndUpdate({ _id: req.user._id },
         { token: "" }, (err, user) => {
-            if(err) return res.json({ success: false, err });
+            if (err) return res.json({ success: false, err });
             return res.status(200).send({
                 success: true
             })

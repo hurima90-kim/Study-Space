e9b0b5651a4b8d40c61dda4ebdc1ad7c74e230d1
Euntae Kim <hurima90@gmail.com>
2021-06-28 00:38:09 +0900

    feat: Create loadmore function

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -7,8 +7,8 @@ function GridCards(props) {
             <div style={{ position: 'relative' }}>
                 <a href={`/movie/${props.movieId}`} >
                     <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
-                    <h2>{props.movieTitle}</h2>
-                    <p>{props.movieDate}</p>
+                    <h2 style={{ fontSize: '18px', margin: '0'}}>{props.movieTitle}</h2>
+                    <p style={{ fontSize: '12px', margin: '0' }}>Release: {props.movieDate}</p>
                 </a>
             </div>
         </Col>

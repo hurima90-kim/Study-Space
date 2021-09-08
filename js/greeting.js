ef91b9e022365ee89d144d5a9f6a8634da48f06f
hurima90-kim <hurima90@gmail.com>
2021-09-08 17:57:23 +0900

    fix: fix greeting message by time

--- js/greeting.js
+++ js/greeting.js
@@ -28,9 +28,9 @@ function paintGreetings(text) {
   greeting.classList.add(SHOWING_CN);
   if (hours >= 6 && hours < 12) {
     greeting.innerText = `Good Morning, ${text}`;
-  } else if (hours >= 12 && hours < 16) {
+  } else if (hours >= 12 && hours < 18) {
     greeting.innerText = `Good Afternoon, ${text}`;
-  } else if (hours >= 18) {
+  } else if (hours >= 18 && hours < 6) {
     greeting.innerText = `Good Evening, ${text}`;
   }
 }

c7a56a46a46f358e349fe95727ae010c547fa57d
hurima90-kim <hurima90@gmail.com>
2021-11-09 18:54:39 +0900

    feat: Fetch API데이터 및 Material-Table 적용

--- src/Pages/BeerList/BeerList.js
+++ src/Pages/BeerList/BeerList.js
@@ -1,14 +1,50 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MaterialTable, { MTableToolbar } from "material-table";
 
-const BeerList = async () => {
-  try {
-    const response = await axios.get("https://api.punkapi.com/v2/beers");
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-  return <div></div>;
+const BeerList = () => {
+  const [posts, setPosts] = useState([]);
+  //   try {
+  //     const response = await axios.get("https://api.punkapi.com/v2/beers");
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("https://api.punkapi.com/v2/beers");
+        setPosts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <MaterialTable
+      title="Beer List"
+      columns={[
+        {
+          title: "Thumbnail",
+          field: "image_Url",
+          render: (rowData) => (
+            <img
+              src={rowData.image_url}
+              style={{ width: 40, borderRadius: "50%" }}
+              alt="img"
+            />
+          ),
+        },
+        { title: "Name", field: "name" },
+        { title: "Tagline", field: "tagline" },
+        { title: "First Brewed", field: "first_brewed" },
+        { title: "Description", field: "description" },
+      ]}
+      data={posts}
+    />
+  );
 };
 
 export default BeerList;

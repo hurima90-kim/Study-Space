db6d1d48d58dc0a528a4ab3a5ac121f1874b1c4d
hurima90-kim <hurima90@gmail.com>
2021-11-09 16:05:06 +0900

    chore: styled-components, styled-reset 패키지 설치 및 BeerList페이지 생성

---
+++ src/Pages/BeerList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BeerList = () => {
+  return <div>BeerList</div>;
+};
+
+export default BeerList;
--- src/Routes.js
+++ src/Routes.js
@@ -5,15 +5,19 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
+import BeerList from "./Pages/BeerList";
 import Home from "./Pages/Home";
+import GlobalStyle from "./Styles/GlobalStyle";
 // import ReactGA from "react-ga";
 
 const Routes = () => {
   return (
     <Router>
+      <GlobalStyle />
       <Switch>
         <Redirect exact from="/" to="/home" />
         <Route path="/home" component={Home} />
+        <Route path="beerlist" component={BeerList} />
       </Switch>
     </Router>
   );

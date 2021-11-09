027422f180064826a63bc3390faddbe39011b245
hurima90-kim <hurima90@gmail.com>
2021-11-09 17:14:45 +0900

    chore: React버전 다운그레이드 및 Material-table 패키지 설치

--- src/Pages/BeerList/BeerList.js
+++ src/Pages/BeerList/BeerList.js
@@ -1,7 +1,14 @@
+import axios from "axios";
 import React from "react";
 
-const BeerList = () => {
-  return <div>BeerList</div>;
+const BeerList = async () => {
+  try {
+    const response = await axios.get("https://api.punkapi.com/v2/beers");
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+  return <div></div>;
 };
 
 export default BeerList;

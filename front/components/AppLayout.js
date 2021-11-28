12215a54596ce2984621dc7eb94e0303cec537f9
hurima90-kim <hurima90@gmail.com>
2021-11-28 22:31:17 +0900

    feat: 더미데이터로 로그인 구현

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,40 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Input, Menu, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
+import styled from "styled-components";
+
+// antd의 컴포넌트에 스타일을 주기위해서
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item>
+        <Menu.Item key="home">
           <Link href="/">
             <a>Node Bird</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="profile">
           <Link href="/profile">
             <a>Profile</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        <Menu.Item key="mail">
+          <SearchInput enterButton />
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="signup">
           <Link href="/signup">
             <a>Sign Up</a>
           </Link>
         </Menu.Item>
       </Menu>
-      <Row>
+      <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽메뉴
+          {isLoggedIn ? (
+            <UserProfile setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <LoginForm setIsLoggedIn={setIsLoggedIn} />
+          )}
         </Col>
         <Col xs={24} md={12}>
           {children}
         </Col>
         <Col xs={24} md={6}>
-          오른쪽메뉴
+          {/* target="_black"를 사용할 때 보안위협을 사라지기 하기 위해 rel속성 작성 */}
+          <a
+            href="https://github.com/hurima90-kim"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by Jason Kim
+          </a>
         </Col>
       </Row>
     </div>
---
+++ front/components/LoginForm.js
@@ -0,0 +1,68 @@
+import React, { useState, useCallback, useMemo } from "react";
+import { Input, Form, Button } from "antd";
+import Link from "next/link";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 컴포넌트에 props를 넘겨주는 함수는 항상 useCallback처리, 그래야 최적화가 된다
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  //   const style = useMemo(() => ({ marginTop: 10 }), []);
+
+  const onSubmitForm = useCallback(() => {
+    //onFinish는 이미 preventDefault가 적용되어있다.
+    console.log(id, password);
+    setIsLoggedIn(true);
+  }, [id, password]);
+
+  return (
+    <FormWrapper onFinish={onSubmitForm}>
+      <div>
+        <label htmlFor="user-id">아이디</label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password">비밀번호</label>
+        <br />
+        <Input
+          name="user-password"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      {/* 스타일 작성할 때 객체로 생성하면 리렌더링이 된다. 객체사용 X */}
+      <ButtonWrapper>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </ButtonWrapper>
+    </FormWrapper>
+  );
+};
+
+export default LoginForm;

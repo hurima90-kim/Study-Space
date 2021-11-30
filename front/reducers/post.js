4c509829e2abf9223d6b66103b591fa475e66b36
hurima90-kim <hurima90@gmail.com>
2021-11-30 22:47:03 +0900

    feat: Post부분 구현(이미지, 댓글 제외)

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,9 +1,67 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "제로초",
+      },
+      content: "첫번째 게시글 #해시태그 #익스프레스",
+      Images: [
+        {
+          src: "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202110/05/2312526e-1340-44a0-8fcc-f31b8b70640c.jpg",
+        },
+        {
+          src: "http://cdnimage.dailian.co.kr/news/202109/news_1631250975_1031941_m_1.jpeg",
+        },
+        {
+          src: "http://cdnimage.dailian.co.kr/news/202109/news_1631250975_1031941_m_1.jpeg",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "nero",
+          },
+          content: "우와 개정판이 나왔군요~",
+        },
+        {
+          User: {
+            nickname: "hero",
+          },
+          content: "얼른 사고 싶어요!",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  content: "더미데이터입니다.",
+  User: {
+    id: 1,
+    nickname: "제로초",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }

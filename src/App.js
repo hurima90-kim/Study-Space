e12c89e8831a6c4c92b4884fdde9308f7b40fed2
hurima90-kim <hurima90@gmail.com>
2022-05-26 23:28:49 +0900

    feat: create useClick hooks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import UseInput from './hooks/useInput';
 import UseTabs from './hooks/useTabs';
 import UseTitle from './hooks/useTitle';
+import UseClick from './hooks/useClick';
 
 function App() {
     return (
         <div className="App">
             {/* <UseInput /> */}
             {/* <UseTabs /> */}
-            <UseTitle />
+            {/* <UseTitle /> */}
+            <UseClick />
         </div>
     );
 }

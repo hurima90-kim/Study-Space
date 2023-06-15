17cd78c2e34b0599d0f7e1246ebc2b955c493dd2
EuntaeKim <hurima90@gmail.com>
2023-06-15 13:51:05 +0900

    feat: create skeleton ui component

---
+++ skeleton/src/components/Skeleton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Skeleton.css";
+
+const Skeleton = () => {
+  return (
+    <li className="skeleton-item">
+      <div>
+        <div className="skeleton-img" />
+      </div>
+      <div className="skeleton-info">
+        <p className="skeleton-name" />
+        <p className="skeleton-email" />
+      </div>
+    </li>
+  );
+};
+
+export default Skeleton;
